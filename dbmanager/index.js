4f0169bb91756ebd7eca04c60c7e2dd3999e3904
GhaidaaShtayeh <GhaidaaShtayeh@hotmail.com>
2021-11-19 09:38:07 +0200

    import csv file into mysql workbench

--- dbmanager/index.js
+++ dbmanager/index.js
@@ -4,16 +4,21 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+//
+const fs = require('fs');
+const csv = require('fast-csv');
+const multer = require('multer')
+const path = require('path')
 //require for our db module 
 
-var mysqlConnection = mysql.createConnection({
+var db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "3amourisraa",
+    password: "0096612122255",
     database: "dbmanager",
     multipleStatements: true
 })
-mysqlConnection.connect((err) => {
+db.connect((err) => {
     if (!err)
         console.log('DB Connected');
     else
@@ -21,10 +26,118 @@ mysqlConnection.connect((err) => {
 });
 app.listen(3000, () => console.log("Server is on port 3000"));
 app.get('/db/files', (req, res) => {
-    mysqlConnection.query('SELECT * FROM files', (err, rows, fields) => {
+    db.query('SELECT * FROM files', (err, rows, fields) => {
         if (!err)
             res.send(rows)
         else
             console.log(err);
     })
 });
+////////////////////////////
+ 
+//use express static folder
+app.use(express.static("./public"))
+ 
+// body-parser middleware use
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+ 
+ 
+db.connect(function (err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+    console.log('Connected to the MySQL server.');
+})
+ 
+//! Use of Multer
+var storage = multer.diskStorage({
+    destination: (req, file, callBack) => {
+        callBack(null, './uploads/')    
+    },
+    filename: (req, file, callBack) => {
+        callBack(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+ 
+var upload = multer({
+    storage: storage
+});
+ 
+//! Routes start
+ 
+//route for Home page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+ 
+//@type   POST
+// upload csv to database
+app.post('/uploadfile', upload.single("uploadfile"), (req, res) =>{
+    UploadCsvDataToMySQL(__dirname + '/uploads/' + req.file.filename);
+    console.log('CSV file data has been uploaded in mysql database ' + err);
+});
+ 
+function UploadCsvDataToMySQL(filePath){
+    let stream = fs.createReadStream(filePath);
+    let csvData = [];
+    let csvStream = csv
+        .parse()
+        .on("data", function (data) {
+            csvData.push(data);
+        })
+        .on("end", function () {
+            // Remove Header ROW
+            csvData.shift();
+  
+            // Open the MySQL connection
+            db.connect((error) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    let query = 'INSERT INTO files (File_ID, File_Name) VALUES ?';
+                    db.query(query, [csvData], (error, response) => {
+                        console.log(error || response);
+                    });
+                }
+            });
+             
+            // delete file after saving to MySQL database
+            // -> you can comment the statement to see the uploaded CSV file.
+            fs.unlinkSync(filePath)
+        });
+  
+    stream.pipe(csvStream);
+}
+ 
+//create connection
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
+
+
+
+
+
+
+/*var csvParser = require('csv').parse;
+
+var filePath ="uploads\\addresses.csv";
+fs.readFile(filePath, {
+  encoding: 'utf-8'
+}, function(err, csvData) {
+  if (err) {
+    console.log(err);
+  }
+
+  csvParser(csvData, {
+    delimiter: ','
+  }, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
+});*/
