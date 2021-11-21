4673ab32cc003e61bcd1b802db8de45ab5b0fd1f
shadabana <79002035+shadabana@users.noreply.github.com>
2021-11-21 19:22:26 +0200

    update in regiser

--- dbmanager/routes.js
+++ dbmanager/routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const {body} = require('express-validator');
 const {register} = require('./controllers/registerController');
 const {login} = require('./controllers/loginController');
-const {getUser} = require('./controllers/getUserController');
+
 
 router.post('/register', [
     body('user_name',"The user_name must be of minimum 3 characters length")
@@ -26,6 +26,6 @@ router.post('/login',[
     body('user_password',"The Password must be of minimum 4 characters length").notEmpty().trim().isLength({ min: 4 }),
 ],login);
 
-router.get('/getuser',getUser);
+
 
 module.exports = router;
