72fda68b1189bbde6b58671e1d2d5f39d7d91914
GhaidaaShtayeh <GhaidaaShtayeh@hotmail.com>
2021-12-11 18:12:30 +0200

    edits and changes

--- dbmanager/dbConnection.js
+++ dbmanager/dbConnection.js
@@ -1,6 +1,12 @@
 const mysqlc=require("mysql2");
+const prompt = require("prompt-sync")({ sigint: true });
 
 //export out db module
+const hostName = prompt("Enter Host Name to connect : ");
+const userName  = prompt("Enter user name to import  : ");
+const password  = prompt("Enter password : ");
+const databaseName = prompt("Enter database name : ");
+console.log(`Your host name is ${hostName} and user name is ${userName} and password is ${password} and database name ${databaseName}`)
 
 const db_connection = mysqlc.createConnection({
         host:"localhost",
